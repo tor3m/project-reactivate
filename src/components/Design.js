4bca7711185bea9f04b6e608f20d00270d141bd4
RosaBF <rosaberned30@gmail.com>
2021-06-07 13:53:20 +0200

    change docs

--- src/components/Design.js
+++ src/components/Design.js
@@ -7,34 +7,33 @@ import Palette from './Palette';
 function Design() {
   return (
     // <fieldset className='user-options'>
-        <div className='color-design'>
-          <div className='color-container'>
-            <p>COLORES</p>
-          </div>
-          <div className='radio-color'>
-            <Palette
-              className = {'js-form js-palette color js-color1'}
-              id = {'color1'}
-              value = {'blue'}
-              checked
-              src = {paletteBlue}
-            ></Palette>
-            <Palette
-              className={'js-form js-palette color js-color2'}
-              id={'color2'}
-              value={'red'}
-              src={paletteRed}
-            ></Palette>
-            <Palette
-              className={'js-form js-palette color js-color3'}
-              id={'color3'}
-              value={'mixedcolor'}
-              src={paletteMixed}
-            ></Palette>   
-            
-          </div>
-        </div>
-      
+    <div className='color-design'>
+      <div className='color-container'>
+        <p>COLORES</p>
+      </div>
+      <div className='radio-color'>
+        <Palette
+          className={'js-form js-palette color js-color1'}
+          id={'color1'}
+          value={'blue'}
+          checked
+          src={paletteBlue}
+        ></Palette>
+        <Palette
+          className={'js-form js-palette color js-color2'}
+          id={'color2'}
+          value={'red'}
+          src={paletteRed}
+        ></Palette>
+        <Palette
+          className={'js-form js-palette color js-color3'}
+          id={'color3'}
+          value={'mixedcolor'}
+          src={paletteMixed}
+        ></Palette>
+      </div>
+    </div>
+
     // </fieldset>
   );
 }
